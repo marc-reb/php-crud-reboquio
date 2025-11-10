1ac0c03c2b5766d641470163cf49d588ec00c945
marc-reb <reboquio.marc@gmail.com>
2025-11-11 02:34:57 +0800

    Added Homepage

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Index</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div class="background-overlay"></div>
+    <h2>Welcome to Student Branch Directory System!</h2>
+    <div class="form-container">
+        <a href="create.php" class="btn">Add Student</a>
+        <a href="read.php" class="btn">View Student</a>
+        <a href="read.php" class="btn">Update Student</a>
+        <a href="read.php" class="btn">Delete Student</a>
+    </div>
+</body>
+</html>
